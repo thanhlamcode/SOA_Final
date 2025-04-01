035ce4ba8434d3ef70dbdfbac01e8f80cb1332fd
thanhlamcode <lamdoan1122334455@gmail.com>
2025-04-01 11:44:06 +0700

    https

--- API/Program.cs
+++ API/Program.cs
@@ -25,7 +25,8 @@ builder.Services.AddCors(options =>
                 "http://localhost:5173",   // Vite mặc định
                 "http://localhost:3000",   // Nếu chạy React trên 3000
                 "http://localhost:3001",    // Nếu chạy Docker frontend dev
-                "https://soa-final-1.onrender.com"
+                "https://soa-final-1.onrender.com",
+                "https://localhost:5173"
             )
             .SetIsOriginAllowed(origin => true) // 🔥 Cần để tránh lỗi CORS khi AllowCredentials
             .AllowAnyMethod()
